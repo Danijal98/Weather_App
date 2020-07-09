66a3fc4ccb9ee63734a4d262197fe82b03dd3f01
Danijal Azerović <danijalazerovic@gmail.com>
2020-07-09 15:46:55 +0200

    Initial commit

---
+++ app/src/main/java/rs/raf/jul/danijal_azerovic_rn8618/MainActivity.kt
@@ -0,0 +1,12 @@
+package rs.raf.jul.danijal_azerovic_rn8618
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
